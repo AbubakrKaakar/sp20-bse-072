87838f9d9cf3343ef0773a452db1c7615eeff9ad
Abubakr <abubakr.kaakar.2016@gmail.com>
2022-04-24 22:37:08 +0500

    Commit1

---
+++ app/src/main/java/com/example/sp20_bse_072assigment2/messageAct.java
@@ -0,0 +1,14 @@
+package com.example.sp20_bse_072assigment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class messageAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
